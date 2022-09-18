1bc763f4dec5d452ec3f80ee01a3c72c6554b5ec
jjoon <jjoony87@gmail.com>
2022-09-18 14:37:44 +0900

    220918_ver1

--- liveon/common.js
+++ liveon/common.js
@@ -4,6 +4,37 @@ window.onload = function(){
     swiper_now();
     nowList();
     swiper_test();
+
+    console.log('aaa');
+
+    const slider = document.querySelector('.scrollBox');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+      slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+    slider.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+        console.log(walk);
+      });
+   
 }
 
 function swiper_banner(){
